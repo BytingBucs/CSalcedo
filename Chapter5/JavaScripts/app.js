1c01fb790d97af8d080ed098212c0b83c653846d
CarmenSal239 <carmenasalcedo@outlook.com>
2016-02-25 16:09:25 -0500

    updated javascript

--- Chapter5/JavaScripts/app.js
+++ Chapter5/JavaScripts/app.js
@@ -1,23 +1,4 @@
-var main = function () {
- "use strict";
- console.log("Hello World!");
- $.getJSON("cards/aceOfSpades.json", function (card) {
- // create an element to hold the card
- var $cardParagraph = $("<p>");
- // create the
- $cardParagraph.text(card.rank + " of " + card.suit);
- // append the card paragraph to main
- $("main").append($cardParagraph);
- });
-}
-$(document).ready(main);
-[
- { "suit" : "spades", "rank" : "ace" },
- { "suit" : "hearts", "rank" : "ten" },
- { "suit" : "spades", "rank" : "five" },
- { "suit" : "clubs", "rank" : "three" },
- { "suit" : "diamonds", "rank" : "three" }
-]
+
 var main = function () {
  "use strict";
  console.log("Hello World!");
