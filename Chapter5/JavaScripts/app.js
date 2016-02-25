595ff5fa2b1fc2c1c7b882518ecb3afb2e6a0485
CarmenSal239 <carmenasalcedo@outlook.com>
2016-02-25 16:00:48 -0500

    Reused the card variable in the forEach loop of the 2nd call to getJSON

--- Chapter5/JavaScripts/app.js
+++ Chapter5/JavaScripts/app.js
@@ -1,10 +1,47 @@
 var main = function () {
  "use strict";
- // getJSON even parses the JSON for us, so we don't need to
- // call JSON.parse
+ console.log("Hello World!");
  $.getJSON("cards/aceOfSpades.json", function (card) {
- // print the card to the console
- console.log(card);
+ // create an element to hold the card
+ var $cardParagraph = $("<p>");
+ // create the
+ $cardParagraph.text(card.rank + " of " + card.suit);
+ // append the card paragraph to main
+ $("main").append($cardParagraph);
+ });
+}
+$(document).ready(main);
+[
+ { "suit" : "spades", "rank" : "ace" },
+ { "suit" : "hearts", "rank" : "ten" },
+ { "suit" : "spades", "rank" : "five" },
+ { "suit" : "clubs", "rank" : "three" },
+ { "suit" : "diamonds", "rank" : "three" }
+]
+var main = function () {
+ "use strict";
+ console.log("Hello World!");
+ $.getJSON("cards/aceOfSpades.json", function (card) {
+ // create an element to hold the card
+ var $cardParagraph = $("<p>");
+ // create the card text
+ $cardParagraph.text(card.rank + " of " + card.suit);
+ // append the card paragraph to main
+ $("main").append($cardParagraph);
+ });
+ $.getJSON("cards/hand.json", function (hand) {
+ var $list = $("<ul>");
+ // hand is an array, so we can iterate over it
+ // using a forEach loop
+ hand.forEach(function (card) {
+ // create a list item to hold the card
+ // and append it to the list
+ var $card = $("<li>");
+ $card.text(card.rank + " of " + card.suit);
+ $list.append($card);
+ });
+ // append the list to main
+ $("main").append($list);
  });
 };
 $(document).ready(main);
