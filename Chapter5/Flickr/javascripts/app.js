efd585c0caf1443caff5c1e91ea6df959bcae9d1
CarmenSal239 <carmenasalcedo@outlook.com>
2016-03-20 18:08:21 -0400

    Iterate over the items object using forEach loop

--- Chapter5/Flickr/javascripts/app.js
+++ Chapter5/Flickr/javascripts/app.js
@@ -5,10 +5,9 @@ var main = function () {
 	// to make it more readable    
 var url = "http://api.flickr.com/services/feeds/photos_public.gne?" +              
 	"tags=dogs&format=json&jsoncallback=?";
-	$.getJSON(url, function (flickrResponse) {        
-	// we'll simply print the response to the console        
-	// for the time being       
-	console.log(flickrResponse);    
+	$.getJSON(url, function (flickrResponse) {    
+	flickrResponse.items.forEach(function (photo) {        
+	console.log(photo.media.m);   
 	}); 
  };
 $(document).ready(main); 
