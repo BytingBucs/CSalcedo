44c0f07fbd2d32d987f24c4c562605e8d843f8cf
CarmenSal239 <carmenasalcedo@outlook.com>
2016-03-20 17:32:10 -0400

    getJSON function

--- Chapter5/Example/javascripts/app.js
+++ Chapter5/Example/javascripts/app.js
@@ -1,9 +1,38 @@
-var main = function () {
- "use strick";
- // get JSON even parses the JSON for us. No need to call JSON.parses
- $.getJSON("cards/aceOfSpades.json",function (card){
- //print the card to the console
- console.log(card);
-});
-};
-$(document).ready(main);
+var main = function () { 
+     "use strict"; 
+ 
+ 
+     $.getJSON("cards/aceOfSpades.json", function (card) { 
+         // create an element to hold the card 6         var $cardParagraph = $("<p>"); 
+ 
+ 
+         // create the  
+        $cardParagraph.text(card.rank + " of " + card.suit); 
+ 
+ 
+         // append the card paragraph to main 
+         $("main").append($cardParagraph); 
+     }); 
+ 
+ 
+     $.getJSON("cards/hand.json", function (hand) { 
+         var $list = $("<ul>"); 
+ 
+ 
+         // hand is an array, so we can iterate over it 
+         hand.forEach(function (card) { 
+             // create a list item to hold the card 
+             // and append it to the list 
+             var $card = $("<li>"); 
+             $card.text(card.rank + " of " + card.suit); 
+            $list.append($card); 
+         }); 
+ 
+ 
+        // append the list to main 
+         $("main").append($list); 
+     }); 
+ }; 
+ 
+ 
+ $(document).ready(main); 
